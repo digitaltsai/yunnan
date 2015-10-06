a0908b1a9001704bc64e18cb8feb356c4530d8e8
digitaltsai <digitaltsai@gmail.com>
2015-10-06 14:53:11 -0700

    changes

--- config.js
+++ config.js
@@ -3,12 +3,16 @@ var config = {
     secret: 'ID_SERVER_SECRET',
   },
 
-  // idServerUrl: 'http://localhost:3000',
-  idServerUrl: 'http://192.168.10.166',
+  idServerUrl: 'http://localhost:3000',
+  //idServerUrl: 'http://192.168.10.166',
 
-  // callbackUrl: 'http://localhost:3001',
-  callbackUrl: 'http://192.168.10.166:3001',
+  baseUrl: 'http://localhost:3001',
+  //baseUrl: 'http://192.168.10.166:3001',
   
+  session: {
+    name: 'yunnan.sid'
+  },
+
   db: {
     mongodb: {
       mongoLabUrl: process.env.MONGOLAB_URI,
